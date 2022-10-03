0f19bf6c479422485bd02e0e57634420de54f051
Prakash A <prakashaathi321@gmail.com>
2022-10-03 12:59:49 +0530

    feature_bot

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,url_for,flash,redirect
+from flask import Flask,render_template,request,url_for,flash,redirect,session
 import sqlite3
 
 app = Flask(__name__)
@@ -11,14 +11,6 @@ con.close()
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/sign-in')
-def signin():
-    return render_template('signin.html')
-
-@app.route('/sign-up')
-def signup():
-    return render_template("signup.html")
 
 @app.route('/about')
 def about():
@@ -55,15 +47,6 @@ def view():
     data=cur.fetchall()
     con.close
     return render_template("view.html",data=data)
-
-@app.route("/signup", methods=['GET', 'POST'])
-def register():
-    return render_template('signup.html')
-
-
-@app.route("/signin", methods=['GET', 'POST'])
-def login():
-    return render_template('signin.html')
 
 @app.route('/update/<string:id>',methods=['POST','GET'])
 def update(id):
@@ -104,6 +87,35 @@ def delete(id):
     finally:
         return redirect (url_for("view"))
         con.close()
+
+# second table for login
+con=sqlite3.connect('database.db')
+con.execute("create table if not exists customer (pid integer primary key,email text,password text)")
+con.close()
+
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        try:
+            email=request.form['email']
+            password=request.form['password']
+            con=sqlite3.connect("database.db")
+            cur=con.cursor()
+            cur.execute("insert into customer (email,password)values(?,?)",(email,password))
+            con.commit()
+            flash("Register successfully",'success')
+        except:
+            flash("error in insert operation",'danger')
+        finally:
+            return redirect(url_for('home'))
+            con.close()
+
+    return render_template("register.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run('0.0.0.0',port=8080,debug=True)
